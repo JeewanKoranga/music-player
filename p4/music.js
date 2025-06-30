32c3ede4d92449a73329ef61a3abacc4aff14d64
jeewan singh koranga <154888787+JeewanKoranga@users.noreply.github.com>
2025-07-01 00:48:25 +0530

    Update music.js

--- p4/music.js
+++ p4/music.js
@@ -12,56 +12,56 @@ const songs = [
     title: "Sajni",
     name: "Arijit Singh, Ram Sampath | Laapataa Ladies | Aamir Khan Productions",
     source:
-      "songs/sajni.mp3",
+      "P4/songs/sajni.mp3",
   },
   {
     title: "Let Her Go x Husn",
     name: " (Gravero Mashup) | Anuv Jains",
     source:
-      "songs/husn.mp3",
+      "P4/songs/husn.mp3",
   },
   {
     title: "SITA SIYA ",
     name: "Lyricist : Hrutul SINGER : Tirth Thakkar",
     source:
-      "songs/sita.mp3",
+      "P4/songs/sita.mp3",
   },
   {
     title: " You Set My World On Fire",
     name: "Loving Caliber (Official Lyric Video)",
     source:
-      "songs/world.mp3",
+      "P4/songs/world.mp3",
   },
   {
     title: "Until I Found You",
     name: "Em Beihold Version",
     source:
-      "songs/until.mp3",
+      "P4/songs/until.mp3",
   },
 
   {
     title: "everything sucks",
     name: "vaultboy speed up version",
     source:
-      "songs/everything.mp3",
+      "P4/songs/everything.mp3",
   },
   {
     title: "KRUSHN AUR KRISHNAA",
     name: "Lyricist : Hrutul SINGER : Tirth Thakkar, Krishani Gadhvi",
     source:
-      "songs/krishna.mp3",
+      "P4/songs/krishna.mp3",
   },
   {
     title: "In My Mind",
     name: "Maximillian - On My Mind (Lyric Video)",
     source:
-      "songs/mind.mp3",
+      "P4/songs/mind.mp3",
   },
   {
     title: "Everything Hit me Like",
     name: "vaultboy - everything hits me at once (Lyrics)",
     source:
-      "songs/every.mp3",
+      "P4/songs/every.mp3",
   },
 ];
 
